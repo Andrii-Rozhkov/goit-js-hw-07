958b24da74d7cd8f0080ef892fef6dadf2bb7d14
Andrii-Rozhkov <152018262+Andrii-Rozhkov@users.noreply.github.com>
2024-05-09 23:35:02 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,4 +1,24 @@
-function getRandomHexColor() {
+const input = document.querySelector('input[type="number"]');
+
+const createBtn = document.querySelector('button[data-create]');
+
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+const boxes = document.querySelector('div[id="boxes]');
+
+let width = 30;
+
+let hight = 30;
+
+
+if (input.value < 1 || input.value > 100) {
+  return "Error";
+} else {
+  1
+}
+
+
+RandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
